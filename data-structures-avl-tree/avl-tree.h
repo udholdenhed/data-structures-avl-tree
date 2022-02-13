35cbe42600365350af5edb3e99b97ede0b451436
udholdenhed <nazarslota03@gmail.com>
2022-02-13 14:11:08 +0200

    Fix of height calculation

--- data-structures-avl-tree/avl-tree.h
+++ data-structures-avl-tree/avl-tree.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <memory>
-#include <string>
 
 namespace ds
 {
@@ -20,20 +19,30 @@ namespace ds
 			avl_tree_node(const key_type& key, const value_type& value);
 
 		public:
-			const key_type& get_key() { return m_key_; }
-			const value_type& get_value() { return m_value_; }
+			key_type get_key() const { return m_key_; }
+			value_type get_value() const { return m_value_; }
 
 		private:
-			std::int32_t get_balance_factor();
+			std::int32_t get_balance_factor() const;
 
-			std::int32_t get_height() const { return m_height_; }
+			void update_height();
+			std::int32_t get_height() const;
 			void set_height(const std::int32_t height) { m_height_ = height; }
 
-			std::shared_ptr<avl_tree_node> get_left() const { return p_left_; }
-			void set_left(const std::shared_ptr<avl_tree_node>& node) { p_left_ = node; }
+			avl_tree_node* get_left() const { return p_left_.get(); }
+			avl_tree_node* get_right() const { return p_right_.get(); }
 
-			std::shared_ptr<avl_tree_node> get_right() const { return p_right_; }
-			void set_right(const std::shared_ptr<avl_tree_node>& node) { p_right_ = node; }
+			std::unique_ptr<avl_tree_node> move_left() { return std::move(p_left_); }
+			std::unique_ptr<avl_tree_node> move_right() { return std::move(p_right_); }
+
+			const avl_tree_node* get_const_left() const { return p_left_.get(); }
+			const avl_tree_node* get_const_right() const { return p_right_.get(); }
+
+			void set_left(const key_type& key, const value_type& value);
+			void set_right(const key_type& key, const value_type& value);
+
+			void set_left(std::unique_ptr<avl_tree_node>&& node) { p_left_ = std::move(node); }
+			void set_right(std::unique_ptr<avl_tree_node>&& node) { p_right_ = std::move(node); }
 
 		private:
 			key_type m_key_{ key_type() };
@@ -41,63 +50,107 @@ namespace ds
 
 		private:
 			std::int32_t m_height_{ 1 };
-			std::shared_ptr<avl_tree_node> p_left_{ nullptr }, p_right_{ nullptr };
+			std::unique_ptr<avl_tree_node> p_left_{ nullptr };
+			std::unique_ptr<avl_tree_node> p_right_{ nullptr };
 		};
 
 	public:
 		avl_tree() = default;
 
 	public:
-		void insert(const key_type& key, const value_type& value);
-		std::shared_ptr<avl_tree_node> search(const key_type& key);
+		bool insert(const key_type& key, const value_type& value);
+		const avl_tree_node* search(const key_type& key) const;
 
 	private:
-		static std::shared_ptr<avl_tree_node> balance(std::shared_ptr<avl_tree_node> node);
+		void balance();
 
-		static std::shared_ptr<avl_tree_node> rotate_left(std::shared_ptr<avl_tree_node> node);
-		static std::shared_ptr<avl_tree_node> rotate_right(std::shared_ptr<avl_tree_node> node);
+		static std::unique_ptr<avl_tree_node> balance(std::unique_ptr<avl_tree_node>&& node);
 
-		static std::shared_ptr<avl_tree_node> rotate_left_right(std::shared_ptr<avl_tree_node> node);
-		static std::shared_ptr<avl_tree_node> rotate_right_left(std::shared_ptr<avl_tree_node> node);
+		static std::unique_ptr<avl_tree_node> rotate_left(std::unique_ptr<avl_tree_node>&& node);
+		static std::unique_ptr<avl_tree_node> rotate_right(std::unique_ptr<avl_tree_node>&& node);
+
+		static std::unique_ptr<avl_tree_node> rotate_left_right(std::unique_ptr<avl_tree_node>&& node);
+		static std::unique_ptr<avl_tree_node> rotate_right_left(std::unique_ptr<avl_tree_node>&& node);
+
 
 	private:
-		std::shared_ptr<avl_tree_node> p_root_{ nullptr };
+		std::unique_ptr<avl_tree_node> p_root_{ nullptr };
 	};
 
-	template <class ValueType, class KeyType>
-	avl_tree<ValueType, KeyType>::avl_tree_node::avl_tree_node(const key_type& key, const value_type& value) :
-		m_key_(key_type(key)), m_value_(value_type(value))
+	template <class KeyType, class ValueType>
+	avl_tree<KeyType, ValueType>::avl_tree_node::avl_tree_node(const key_type& key, const value_type& value) :
+		m_key_(key), m_value_(value_type(value))
 	{
 	}
 
 	template <class KeyType, class ValueType>
-	std::int32_t avl_tree<KeyType, ValueType>::avl_tree_node::get_balance_factor()
+	std::int32_t avl_tree<KeyType, ValueType>::avl_tree_node::get_balance_factor() const
 	{
-		const std::int32_t left_height = p_left_ != nullptr ? p_left_->m_height_ : 0;
-		const std::int32_t right_height = p_right_ != nullptr ? p_right_->m_height_ : 0;
+		const std::int32_t left_height = p_left_ != nullptr ? p_left_->get_height() : 0;
+		const std::int32_t right_height = p_right_ != nullptr ? p_right_->get_height() : 0;
 		return right_height - left_height;
 	}
 
-	template <class ValueType, class KeyType>
-	void avl_tree<ValueType, KeyType>::insert(const key_type& key, const value_type& value)
+	template <class KeyType, class ValueType>
+	void avl_tree<KeyType, ValueType>::avl_tree_node::update_height()
+	{
+		std::int32_t left_height = 0, right_height = 0;
+		if (p_left_ != nullptr)
+		{
+			p_left_->update_height();
+			left_height = p_left_->get_height();
+		}
+
+		if (p_right_ != nullptr)
+		{
+			p_right_->update_height();
+			right_height = p_right_->get_height();
+		}
+
+		m_height_ = std::max(left_height, right_height) + 1;
+	}
+
+	template <class KeyType, class ValueType>
+	std::int32_t avl_tree<KeyType, ValueType>::avl_tree_node::get_height() const
+	{
+		const std::int32_t left_height = p_left_ != nullptr ? p_left_->get_height() : 0;
+		const std::int32_t right_height = p_right_ != nullptr ? p_right_->get_height() : 0;
+		return std::max(left_height, right_height) + 1;
+	}
+
+	template <class KeyType, class ValueType>
+	void avl_tree<KeyType, ValueType>::avl_tree_node::set_left(const key_type& key, const value_type& value)
+	{
+		p_left_ = std::unique_ptr<avl_tree_node>(new avl_tree_node(key, value));
+	}
+
+	template <class KeyType, class ValueType>
+	void avl_tree<KeyType, ValueType>::avl_tree_node::set_right(const key_type& key, const value_type& value)
+	{
+		p_right_ = std::unique_ptr<avl_tree_node>(new avl_tree_node(key, value));
+	}
+
+	template <class KeyType, class ValueType>
+	bool avl_tree<KeyType, ValueType>::insert(const key_type& key, const value_type& value)
 	{
 		if (p_root_ == nullptr)
 		{
-			p_root_ = std::make_shared<avl_tree_node>(key, value);
-			return;
+			p_root_ = std::unique_ptr<avl_tree_node>(new avl_tree_node(key, value));
+			return true;
 		}
 
-		std::shared_ptr<avl_tree_node> node = p_root_;
+		avl_tree_node* node = p_root_.get();
 		while (true)
 		{
-			// if (key == node->get_key())
-			// 	throw std::exception("the value with this key is already in the tree");
+			const key_type node_key = node->get_key();
+			if (key == node_key)
+				return false;
 
-			if (key < node->get_key())
+			if (key < node_key)
 			{
-				if (node->get_left() == nullptr)
+				if (node->get_const_left() == nullptr)
 				{
-					node->set_left(std::make_shared<avl_tree_node>(key, value));
+					node->set_left(key, value);
 					break;
 				}
 
@@ -105,122 +158,112 @@ namespace ds
 				continue;
 			}
 
-			if (node->get_right() == nullptr)
+			if (node->get_const_right() == nullptr)
 			{
-				node->set_right(std::make_shared<avl_tree_node>(key, value));
+				node->set_right(key, value);
 				break;
 			}
 
 			node = node->get_right();
 		}
 
-		//const std::int32_t left_height = node->get_left() != nullptr ? node->get_left()->get_height() : 0;
-		//const std::int32_t right_height = node->get_right() != nullptr ? node->get_right()->get_height() : 0;
-		//node->set_height(std::max(left_height, right_height) + 1);
+		p_root_->update_height();
+		balance();
 
-		//const std::int32_t balance_factor = p_root_->get_balance_factor();
-		//if (balance_factor < -1 || balance_factor > 1)
-		//	p_root_ = balance(p_root_);
+		return true;
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::search(
-		const key_type& key)
+	const typename avl_tree<KeyType, ValueType>::avl_tree_node* avl_tree<KeyType, ValueType>::search(
+		const key_type& key) const
 	{
-		std::shared_ptr<avl_tree_node> node = p_root_;
+		const avl_tree_node* node = p_root_.get();
 		while (node != nullptr)
 		{
 			if (node->get_key() == key)
-				return std::make_shared<avl_tree_node>(node->get_key(), node->get_value());
+				return node;
 
-			node = key < node->get_key() ? node->get_left() : node->get_right();
+			node = key < node->get_key() ? node->get_const_left() : node->get_const_right();
 		}
 
 		return nullptr;
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::balance(
-		std::shared_ptr<avl_tree_node> node)
+	void avl_tree<KeyType, ValueType>::balance()
+	{
+		if (!(p_root_->get_balance_factor() < -1 || p_root_->get_balance_factor() > 1))
+			return;
+
+		p_root_ = balance(std::move(p_root_));
+	}
+
+	template <class KeyType, class ValueType>
+	std::unique_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::balance(
+		std::unique_ptr<avl_tree_node>&& node)
 	{
 		const std::int32_t balance_factor = node->get_balance_factor();
 		const std::int32_t left_balance_factor = node->get_left() != nullptr ? node->get_left()->get_balance_factor() : 0;
 		const std::int32_t right_balance_factor = node->get_right() != nullptr ? node->get_right()->get_balance_factor() : 0;
 		if (balance_factor < -1 && left_balance_factor == -1)
-			return rotate_right(node);
+			return rotate_right(std::move(node));
 		else if (balance_factor > 1 && right_balance_factor == 1)
-			return rotate_left(node);
+			return rotate_left(std::move(node));
 		else if (balance_factor < -1 && left_balance_factor == 1)
-			return rotate_left_right(node);
+			return rotate_left_right(std::move(node));
 		else if (balance_factor > 1 && right_balance_factor == -1)
-			return rotate_right_left(node);
+			return rotate_right_left(std::move(node));
 
-		return node;
+		return std::move(node);
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_left(
-		std::shared_ptr<avl_tree_node> node)
+	std::unique_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_left(
+		std::unique_ptr<avl_tree_node>&& node)
 	{
-		std::shared_ptr<avl_tree_node> right = node->get_right();
-		node->set_right(right->get_left());
-		right->set_left(node);
+		std::unique_ptr<avl_tree_node> right = node->move_right();
+		node->set_right(std::move(right->move_left()));
+		node->set_height(node->get_height());
 
-		std::int32_t left_height = node->get_left() != nullptr ? node->get_left()->get_height() : 0;
-		std::int32_t right_height = node->get_right() != nullptr ? node->get_right()->get_height() : 0;
-		node->set_height(std::max(left_height, right_height) + 1);
+		right->set_left(std::move(node));
+		right->set_height(right->get_height());
 
-		left_height = right->get_left() != nullptr ? right->get_left()->get_height() : 0;
-		right_height = right->get_right() != nullptr ? right->get_right()->get_height() : 0;
-		right->set_height(std::max(left_height, right_height) + 1);
-
-		return right;
+		return std::move(right);
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_right(
-		std::shared_ptr<avl_tree_node> node)
+	std::unique_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_right(
+		std::unique_ptr<avl_tree_node>&& node)
 	{
-		std::shared_ptr<avl_tree_node> left = node->get_left();
-		node->set_left(left->get_right());
-		left->set_right(node);
+		std::unique_ptr<avl_tree_node> left = node->move_left();
+		node->set_left(left->move_right());
+		node->set_height(node->get_height());
 
-		std::int32_t left_height = node->get_left() != nullptr ? node->get_left()->get_height() : 0;
-		std::int32_t right_height = node->get_right() != nullptr ? node->get_right()->get_height() : 0;
-		node->set_height(std::max(left_height, right_height));
+		left->set_right(std::move(node));
+		left->set_height(left->get_height());
 
-		left_height = left->get_left() != nullptr ? left->get_left()->get_height() : 0;
-		right_height = left->get_right() != nullptr ? left->get_right()->get_height() : 0;
-		left->set_height(std::max(left_height, right_height) + 1);
-
-		return left;
+		return std::move(left);
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::
-		rotate_left_right(std::shared_ptr<avl_tree_node> node)
+	std::unique_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_left_right(
+		std::unique_ptr<avl_tree_node>&& node)
 	{
-		node->set_left(rotate_left(node->get_left()));
-		node = rotate_right(node);
+		node->set_left(rotate_left(node->move_left()));
+		node = rotate_right(std::move(node));
+		node->set_height(node->get_height());
 
-		const std::int32_t left_height = node->get_left() != nullptr ? node->get_left()->get_height() : 0;
-		const std::int32_t right_height = node->get_right() != nullptr ? node->get_right()->get_height() : 0;
-		node->set_height(std::max(left_height, right_height) + 1);
-
-		return node;
+		return std::move(node);
 	}
 
 	template <class KeyType, class ValueType>
-	std::shared_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::
-		rotate_right_left(std::shared_ptr<avl_tree_node> node)
+	std::unique_ptr<typename avl_tree<KeyType, ValueType>::avl_tree_node> avl_tree<KeyType, ValueType>::rotate_right_left(
+		std::unique_ptr<avl_tree_node>&& node)
 	{
-		node->set_right(rotate_right(node->get_right()));
-		node = rotate_left(node);
+		node->set_right(rotate_right(node->move_right()));
+		node = rotate_left(std::move(node));
+		node->set_height(node->get_height());
 
-		const std::int32_t left_height = node->get_left() != nullptr ? node->get_left()->get_height() : 0;
-		const std::int32_t right_height = node->get_right() != nullptr ? node->get_right()->get_height() : 0;
-		node->set_height(std::max(left_height, right_height) + 1);
-
-		return node;
+		return std::move(node);
 	}
 } // DS
--- data-structures-avl-tree/main.cpp
+++ data-structures-avl-tree/main.cpp
@@ -5,33 +5,23 @@
 
 #include <iostream>
 
-#define NUMBER_OF_RANDOM_NUMBERS 50
+#define NUMBER_OF_RANDOM_NUMBERS 100
 
 int main(int argc, char* argv[])
 {
 	std::random_device random_device;
 	std::mt19937 mt19937(random_device());
 	const std::uniform_int_distribution<std::int32_t> int8_distribution(INT8_MIN, INT8_MAX);
-
 	std::vector<std::int32_t> random_numbers(NUMBER_OF_RANDOM_NUMBERS);
 	for (auto& random_number : random_numbers)
 		random_number = int8_distribution(mt19937);
 
-	// const std::vector<std::int32_t> random_numbers = { 47, -20, -35, -108, 110, -49, 64, -101, 24, 23 };
 	ds::avl_tree<std::int32_t, std::string> avl_tree;
 	for (const auto& random_number : random_numbers)
 		avl_tree.insert(random_number, std::to_string(random_number));
 
 	for (const auto& random_number : random_numbers)
-	{
-		if (avl_tree.search(random_number) != nullptr)
-			continue;
-
-		std::cout << "ERROR";
-		return EXIT_FAILURE;
-	}
-
-
+		std::cout << avl_tree.search(random_number)->get_value() << std::endl;
 
 	return EXIT_SUCCESS;
 }
